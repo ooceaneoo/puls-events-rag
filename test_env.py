6a31d9a22626e21ab372e2e69e939661e13ec15f
Océane Dehail <oceane.dehail@gmail.com>
2026-05-03 23:47:06 +0200

    Ajout de l’indexation vectorielle avec Faiss

---
+++ test_env.py
@@ -0,0 +1,6 @@
+import faiss
+from langchain_community.vectorstores import FAISS
+from langchain_mistralai import MistralAIEmbeddings
+import fastapi
+
+print("Environnement configuré avec succès !")
